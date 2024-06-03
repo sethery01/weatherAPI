4577b09025e4e8eb6b9ce7f265b47bde7b061374
Seth Ek <seth.ek@veteransunted.com>
2024-06-03 15:58:43 -0500

    Version 2.0

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app  = Flask(__name__)
 def serve_static_html():
     return send_from_directory('./', 'index.html')
 
-@app.route('/weather')
+@app.route('/weather', methods=["GET"])
 def consume_state_weather():
     state = request.args.get("state")
 
@@ -30,13 +30,15 @@ def consume_state_weather():
     response = requests.get("https://api.weather.gov/alerts/active/area/" + state)
 
     if response.status_code == 200:
-        alerts = []
+        #alerts = []
+        alerts_dict = {}
         for data in response.json()["features"]:
-            alerts.append(data["properties"]["headline"])
-            alerts.append(data["properties"]["instruction"])
-        return alerts, 200
+            alerts_dict[data["properties"]["headline"]] = data["properties"]["instruction"]
+            #alerts.append(data["properties"]["instruction"])
+
+        return alerts_dict, 200
     else:
         return "ERROR 404 NOT FOUND", 404
     
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(host = "0.0.0.0", port=80)
